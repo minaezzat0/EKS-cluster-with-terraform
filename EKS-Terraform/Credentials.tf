b9f50b77047c366c091a11c92a097c79dd50f843
minaezzat0 <99493174+minaezzat0@users.noreply.github.com>
2023-04-09 13:11:37 +0200

    Update Credentials.tf

--- EKS-Terraform/Credentials.tf
+++ EKS-Terraform/Credentials.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region                   = var.aws_region
-  shared_credentials_files = ["/Users/minaezzat/.aws/credentials"]
-  profile                  = "default"
+  shared_credentials_files = ["/Users/minaezzat/.aws/credentials"]    // path of your aws_credentials_file
+  profile                  = "default"                                // name of profile you want to use
 }
